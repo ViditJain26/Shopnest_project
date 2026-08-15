1c6a6997d55ca281a875fb47bb5c3bc3e9fbcde3
ViditJain26 <viditjain44gaya@gmail.com>
2026-08-16 01:33:30 +0530

    Set dns default result order to ipv4first

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+// Force IPv4 resolution to prevent ENETUNREACH on Render
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
